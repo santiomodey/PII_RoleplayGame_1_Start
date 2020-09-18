eacabed24d396403f13c28a69b7d9031c88ba22c
Santiago García <scgc612@gmail.com>
2020-09-18 00:37:50 -0300

    Algun cambio

--- src/Test/Library.Test/DwarfTest.cs
+++ src/Test/Library.Test/DwarfTest.cs
@@ -30,6 +30,9 @@ namespace Test.Library
             Assert.AreEqual(Dwarf20.Name, "Dwarf1");
         }
 
+        /// <summary>
+        /// Este test verifica que el metodo ReceiveAttack funcione correctamente
+        /// </summary>
         [Test]
         public void Test2()
         {
@@ -37,6 +40,10 @@ namespace Test.Library
             Assert.AreEqual(Dwarf20.Health, 80);
         }
 
+
+        /// <summary>
+        /// Este metodo verifica que cuando la vida sea negativa la vida se setee a 0
+        /// </summary>
         [Test]
         public void Test3()
         {
@@ -45,16 +52,47 @@ namespace Test.Library
             Assert.AreEqual(Dwarf20.Health, 0);
         } 
         
+
+        /// <summary>
+        /// Este test verifica que el valor del Attack sea sumado con el valor del elemento
+        /// agregado en el constructor del objeto (se crea con el objeto Guante)
+        /// </summary>
         [Test]
         public void Test4()
         {
             Assert.AreEqual(Dwarf20.Attack, 90);
         }
 
+        /// <summary>
+        /// Este test verifica que se pueda obtener el nombre del elemento que tiene
+        /// el objeto Dwarf20 con el que fue creado
+        /// </summary>
         [Test]
         public void Test5()
         {
             Assert.AreEqual(Dwarf20.Guante1.Name, "Guante1");
+        }
+        
+        /// <summary>
+        /// Este test verifica que al aplicarse el metodo curar 
+        /// la vida del objeto sea seteada a 100
+        /// </summary>
+        [Test]
+        public void Test6()
+        {
+            Dwarf20.ReceiveAttack(Dwarf30);
+            Dwarf20.Curar();
+            Assert.AreEqual(Dwarf20.Health, 100 );
+        }
+        /// <summary>
+        /// Este test verifica que al aplicarse el metodo curar 
+        /// cuando la vida del objeto no sea menor a 100 quede con la vida que está
+        /// </summary>
+        [Test]
+        public void Test7()
+        {
+            Dwarf20.Curar();
+            Assert.AreEqual(Dwarf20.Health, 170);
         }
     }
 }
